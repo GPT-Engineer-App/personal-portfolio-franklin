22cf7c1ed69b415c7b77fa7336330913c47c13a4
Kristian Kyvik <kristianruizkyvik@gmail.com>
2024-04-16 13:02:05 +0200

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -8,7 +8,7 @@ const Index = () => {
       <Container maxW="container.lg" py={8}>
         <VStack spacing={8} align="stretch">
           <Heading as="h1" size="2xl" textAlign="center">
-            Franklin Piercing - hhahahha
+            Franklin Piercing - change versino 222222
           </Heading>
           <Image src="https://images.unsplash.com/photo-1585075712979-cd3746455fcf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxmcmFua2xpbiUyMHBpZXJjZSUyMHBvcnRyYWl0fGVufDB8fHx8MTcxMzI2MTI5M3ww&ixlib=rb-4.0.3&q=80&w=1080" alt="Franklin Pierce" objectFit="cover" borderRadius="full" boxSize="300px" alignSelf="center" />
           <Text fontSize="xl" textAlign="center">
